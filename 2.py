173dae5233ef4ab16a732f9d7a13b301edac3bcb
Dharani Nivash <118915442+dharaninivash@users.noreply.github.com>
2025-06-23 10:05:35 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,13 @@
+# Creating a list
+my_list = [10, 20, 30, 40, 50]
+print("Original List:", my_list)
+
+# Manipulate list
+my_list.append(60)
+my_list.insert(2, 25)
+my_list.remove(40)
+print("Manipulated List:", my_list)
+
+# Slicing list
+print("First 3 elements:", my_list[:3])
+print("Last 2 elements:", my_list[-2:])
